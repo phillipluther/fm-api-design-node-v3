62a29d680382a43740198358c5282fb3c723ae00
Phillip Luther <dev@phillipluther.com>
2022-03-21 06:26:47 -0700

    Lesson 2

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,15 @@ import { Router } from 'express'
 
 const router = Router()
 
+router
+  .route('/')
+  .get((req, res) => {})
+  .post((req, res) => {})
+
+router
+  .route('/:id')
+  .get((req, res) => {})
+  .put((req, res) => {})
+  .delete((req, res) => {})
+
 export default router
