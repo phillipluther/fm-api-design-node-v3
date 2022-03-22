6c55048c4f9c68dc14faffd9b16bf1dcd4744773
Phillip Luther <dev@phillipluther.com>
2022-03-22 07:13:10 -0700

    Lesson 5

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -16,10 +16,68 @@ export const verifyToken = token =>
     })
   })
 
-export const signup = async (req, res) => {}
+export const signup = async (req, res) => {
+  const { email, password } = req.body
 
-export const signin = async (req, res) => {}
+  if (!email || !password) {
+    return res.status(400).send({ message: 'unauthorized' })
+  }
+
+  const user = await User.create(req.body)
+  const token = newToken(user)
+
+  return res.status(201).send({ token })
+}
+
+export const signin = async (req, res) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    return res.status(400).send({ message: 'unauthorized' })
+  }
+
+  const user = await User.findOne({ email }).exec()
+
+  if (!user) {
+    return res.status(401).send({ message: 'unauthorized' })
+  }
+
+  try {
+    const match = await user.checkPassword(password)
+
+    if (!match) {
+      return res.status(401).send({ message: 'not auth' })
+    }
+  } catch (err) {
+    console.log(err)
+    return res.status(400).send({ message: 'not auth' })
+  }
+
+  const token = newToken(user)
+  return res.status(201).send({ token })
+}
 
 export const protect = async (req, res, next) => {
-  next()
+  if (!req.headers.authorization) {
+    res.status(401).send({ message: 'no auth' })
+  }
+
+  let token = req.headers.authorization.split('Bearer ')[1]
+
+  if (!token) {
+    return res.status(401).send({ message: 'nope' })
+  }
+
+  try {
+    const payload = await verifyToken(token)
+    const user = User.findById({ id: payload.id })
+      .select('-password')
+      .exec()
+
+    req.user = user
+    next()
+  } catch (err) {
+    console.error(err)
+    return res.status(401).send({ message: 'nope' })
+  }
 }
